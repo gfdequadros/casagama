f6ba534924fd8b2d9e476e2e460f434beb8ab5ef
gfdequadros <gfdequadros@gmail.com>
2026-04-30 19:15:31 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCh6v-494lI32l5MFYrsHB4UfAjEU0OEio",
+    authDomain: "gama-arq.firebaseapp.com",
+    projectId: "gama-arq",
+    storageBucket: "gama-arq.firebasestorage.app",
+    messagingSenderId: "182305882567",
+    appId: "1:182305882567:web:7f0306ad39d0c965cd59e2"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
