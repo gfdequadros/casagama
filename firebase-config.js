b8624d9db2fc1e8f34b2c6d0a2fedd9393acfb6d
gfdequadros <gfdequadros@gmail.com>
2026-04-30 22:12:26 -0300

    -

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,1 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyA8MjrgMnWV202EbpNSFS89sBmXHr5AffA",
-  authDomain: "gamaarq.firebaseapp.com",
-  projectId: "gamaarq",
-  storageBucket: "gamaarq.firebasestorage.app",
-  messagingSenderId: "725535528813",
-  appId: "1:725535528813:web:7c5f9fabca67b89d5a6c64"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
